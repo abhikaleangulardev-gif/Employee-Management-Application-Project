b38966ffba7713bb3299bb035e52b131c9b4e220
abhishek.kale <abhisahebpatil@gmail.com>
2026-01-03 19:30:19 +0530

    feature/employee-dashboard-021

--- src/app/component/employee-dashboard/employee-dashboard.ts
+++ src/app/component/employee-dashboard/employee-dashboard.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLinkActive, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-employee-dashboard',
-  imports: [RouterOutlet],
+  imports: [CommonModule,RouterOutlet,RouterLink, RouterLinkActive],
   templateUrl: './employee-dashboard.html',
   styleUrl: './employee-dashboard.css',
 })
